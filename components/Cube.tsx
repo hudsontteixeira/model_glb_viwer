cab529c1a6e4a27cae2b8550040cdcd63bc0944d
diebraga <diebraga0055@gmail.com>
2021-12-20 16:49:45 +0000

    up

--- components/Cube.tsx
+++ components/Cube.tsx
@@ -3,10 +3,10 @@ import { useGLTF, useTexture } from '@react-three/drei'
 import { useSpring, a } from '@react-spring/three'
 
 export function Cube() {
-  const { nodes } = useGLTF('/level.glb') as any
+  const { nodes, material } = useGLTF('/level.glb') as any
   const [materialColor, setMaterialColor] = useState('pink')
   const matcap = useTexture(`/${materialColor}.jpg`)
-
+  
   const [hovered, setHovered] = useState(false)
 
   useEffect(() => {
@@ -36,6 +36,7 @@ export function Cube() {
       onPointerOver={() => setHovered(true)}
       onPointerOut={() => setHovered(false)} 
       onClick={changeMaterialColor}
+      
     >
       <mesh geometry={nodes.Cube.geometry} position={[-0.8, 1.24, 0.61]}>
         <meshMatcapMaterial matcap={matcap} />
